4fe27b58d858b24ed62e7929e18ff3e74b2bdad8
我是一坨牛粪 <15011258339@163.com>
2019-02-11 22:50:14 +0800

    1、天机NavBar（报错：TypeError: null is not an object (evaluating 'RNGestureHandlerModule.State'）下载个依赖npm install --save react-native-gesture-handler  react-native link，如果不行先移除依赖包重现你怕吗即可

--- Myapp/AppContainer.js
+++ Myapp/AppContainer.js
@@ -5,9 +5,25 @@ import {createAppContainer,createBottomTabNavigator,createStackNavigator}from 'r
 import Home from './Home';
 import Setting from './Setting';
 
+const Homestack = createStackNavigator({
+    Home:{
+        screen:Home,
+        navigationOptions:{
+            headerTitle:"首页"
+        }
+    }
+})
+const SettingStack = createStackNavigator({
+    Setting:{
+        screen:Setting,
+        navigationOptions:{
+            headerTitle:"设置"
+        }
+    }
+})
 const TabBar = createBottomTabNavigator({
-    Home:{screen:Home},
-    Setting:{screen:Setting}
+    Home:{screen:Homestack},
+    Setting:{screen:SettingStack}
 })
 
 export default  AppContainer = createAppContainer(TabBar);
