33c7021ddaa8b1fac38cfe8a36f14d68e8224ee9
lipk <lipk@linkface.cn>
2019-02-13 15:00:32 +0800

    1、添加底部tabBar图片， 2、添加轮播库（npm install react-native-looped-carousel -S）

--- Myapp/AppContainer.js
+++ Myapp/AppContainer.js
@@ -1,29 +1,111 @@
-import React, {Component}from 'react';
-import {View,Text,StyleSheet} from 'react-native';
-import {createAppContainer,createBottomTabNavigator,createStackNavigator}from 'react-navigation';
-
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {createAppContainer, createBottomTabNavigator, createStackNavigator} from 'react-navigation';
+import Mine from './MineView';
 import Home from './Home';
 import Setting from './Setting';
 
-const Homestack = createStackNavigator({
-    Home:{
-        screen:Home,
-        navigationOptions:{
-            headerTitle:"首页"
+const HomeStack = createStackNavigator({
+    Home: {
+        screen: Home,
+        navigationOptions: {
+            headerTitle: "首页",
+            headerTitleStyle: {
+                flag: 1,
+                textAlign: 'center',
+            },
         }
     }
 })
 const SettingStack = createStackNavigator({
-    Setting:{
-        screen:Setting,
-        navigationOptions:{
-            headerTitle:"设置"
+    Setting: {
+        screen: Setting,
+        navigationOptions: {
+            headerTitle: "电影列表",
+
         }
     }
 })
+
+const MineView = createStackNavigator({
+    Mine: {
+        screen: Mine,
+        navigationOptions: {
+            headerTitle: '我的',
+            headerTitleStyle: {
+                flag: 1,
+                textAlign: 'center',
+            }
+        }
+    }
+
+})
 const TabBar = createBottomTabNavigator({
-    Home:{screen:Homestack},
-    Setting:{screen:SettingStack}
+    // 首页
+    Home: {
+        screen: HomeStack,
+        navigationOptions: {
+            tabBarLabel: "首页",
+            tabBarIcon:({focused,tintColor})=>{
+                return(
+                    <Image
+                    source={
+                        focused
+                        ?require("./images/tarBar/home_normal.png")
+                            :require("./images/tarBar/home_selected.png")
+                    }
+                    style={{tintColor:tintColor,width:24,height:24}}
+                    />
+                )
+            }
+        },
+    },
+// 电影列表
+    MoviesList: {screen: SettingStack,
+        navigationOptions:{
+        tabBarLabel:'电影',
+            tabBarIcon:({focused,tintColor})=>{
+            return(
+            <Image
+            source={
+                focused
+                ?require('./images/tarBar/movie_normal.png')
+                    :require('./images/tarBar/movie_selected.png')
+            }
+            style={{tintColor:tintColor,width:24,height:24}}
+            />)
+            }
+        }
+    },
+    //我的页面
+    Mine: {
+        screen: MineView,
+        navigationOptions:{
+            tabBarLabel:"我的",
+            tabBarIcon:({focused,tintColor})=>{
+                return(
+                    <Image
+                    source={
+                        focused
+                        ?require('./images/tarBar/mine_normal.png')
+                            :require('./images/tarBar/mine_selected.png')
+                    }
+                    style={{tintColor:tintColor,width:24,height:24}}
+                    />
+                )
+
+            }
+        }
+    },
+
+},
+    {
+        tabBarOptions:{
+            activeTintColor:"red",
+            inactiveTintColor:"black"
+        }
+
 })
 
-export default  AppContainer = createAppContainer(TabBar);
+
+export default AppContainer = createAppContainer(TabBar);
