82f76a27b25e7fc135ad0542b3ce151eaf116947
lipk <lipk@linkface.cn>
2019-02-16 16:56:20 +0800

    1、添加上拉加载下来加载更多组件 2、添加电影详情列表

--- Myapp/RealMovieList.js
+++ Myapp/RealMovieList.js
@@ -1,15 +1,27 @@
 import React,{Component} from 'react';
-import {View,Text} from'react-native';
+import {View,Text,} from'react-native';
+import RefreshListVIew,{RefreshState}from 'react-native-refresh-list-view'
 
 export default  class RealMovieList extends Component{
 
+    constructor(props){
+        super(props);
+        this.state={
+            MoviesList:[],
+            refreshState:RefreshState.Idle,
+            movieType:props.navigation.getParam('movieType'),
+            pageIndex:1,
+            pageSize:10,
+            total:0,
+            isLoading:true,
+        }
+    }
+
     render(){
         return(
-            <View>
-                <Text>
-                    {this.props.navigation.getParam("title")}
-                </Text>
-            </View>
+            <RefreshListVIew
+
+            />
         )
     }
 }
